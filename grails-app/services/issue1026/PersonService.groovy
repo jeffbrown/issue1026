79cecb067a41e65ae2a3c93b90d5a585d064049f
Jeff Scott Brown <brownj@objectcomputing.com>
2017-11-18 15:17:53 -0600

    This will cause one of the integration tests to fail.

--- grails-app/services/issue1026/PersonService.groovy
+++ grails-app/services/issue1026/PersonService.groovy
@@ -1,18 +1,19 @@
 package issue1026
 
+import grails.gorm.DetachedCriteria
 import grails.gorm.transactions.Transactional
 
 @Transactional(readOnly = true)
 class PersonService {
 
     Number countPeopleThatHaveAFirstName() {
-        Person.where {
+        new DetachedCriteria(Person).where {
             firstName != null
         }.count()
     }
 
     Number countPeopleWithNoFirstName() {
-         Person.where {
+         new DetachedCriteria(Person).where {
             firstName == null
         }.count()
     }
